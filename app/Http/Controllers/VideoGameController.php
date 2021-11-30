f6db718e88eb242d3b7d2936d62d82cb806dafdf
Samora Machel <samora@me.laptop>
2021-11-30 23:45:32 +0300

    Created controllers for the models

---
+++ app/Http/Controllers/VideoGameController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\VideoGame;
+use Illuminate\Http\Request;
+
+class VideoGameController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\VideoGame  $videoGame
+     * @return \Illuminate\Http\Response
+     */
+    public function show(VideoGame $videoGame)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\VideoGame  $videoGame
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(VideoGame $videoGame)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\VideoGame  $videoGame
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, VideoGame $videoGame)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\VideoGame  $videoGame
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(VideoGame $videoGame)
+    {
+        //
+    }
+}
