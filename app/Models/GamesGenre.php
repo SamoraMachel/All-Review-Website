902b1b94445ed4a1d049e7dc0909078d31870b23
Samora Machel <samora@me.laptop>
2021-11-30 23:27:59 +0300

    Added genre for music and video games

---
+++ app/Models/GamesGenre.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GamesGenre extends Model
+{
+    use HasFactory;
+}
